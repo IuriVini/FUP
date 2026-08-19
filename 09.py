a97ca3ed7bd064213ef6950dc976bb106d2b27bf
Iuri Vinícius <167722693+IuriVini@users.noreply.github.com>
2026-08-19 20:52:42 -0300

    Add angle conversion from degrees to radians
    Implement angle conversion from degrees to radians.

---
+++ 09.py
@@ -0,0 +1,4 @@
+#Leia um ângulo em graus e apresente-o convertido em radianos. A fórmula de conversão é: R = G * π/180 , sendo G o ângulo em graus e R em radianos.
+g = float(input())
+r = g * 3.14 / 180
+print(f'{r:.2f}')
