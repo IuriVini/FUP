0e54047f8691e8f169ead2cd001e14fc402d1760
Iuri Vinícius <167722693+IuriVini@users.noreply.github.com>
2026-08-19 20:53:16 -0300

    Add salary increase calculation program
    Implement salary increase calculation

---
+++ 11.py
@@ -0,0 +1,4 @@
+#Faça um programa que receba o salário de um funcionário. Calcule e imprima o valor do novo salário, sabendo que ele recebeu um aumento de 21,37 %.
+s = float(input())
+ns = s * 1.2137
+print(f'{ns:.2f}')
